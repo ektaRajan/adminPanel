e7034ed6742a1925676573eb9d993023aa03bb34
ektaRajan <ektasirodariya19@gmail.com>
2025-05-01 21:59:30 +0530

    add new components nd create sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AppComponent } from './app.component';
+import { VehicleComponent } from './components/vehicle/vehicle.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'vehicle', component: VehicleComponent},
+
+
+];
